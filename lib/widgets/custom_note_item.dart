43db52f223b1d4e7d1a9051226b6ecda45096f92
Eng_Mosad <mohammedmosaad869@gmail.com>
2024-01-17 01:05:46 +0200

    create custom note item

---
+++ lib/widgets/custom_note_item.dart
@@ -0,0 +1,70 @@
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(
+        top: 24,
+        bottom: 24,
+        left: 16,
+      ),
+      decoration: BoxDecoration(
+        color: const Color(0xffffcc80),
+        borderRadius: BorderRadius.circular(
+          16,
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title:const Text(
+              'Flutter Tips',
+              style: TextStyle(
+                fontSize: 26,
+                color: Colors.black,
+              ),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(
+                top: 16,
+                bottom: 16,
+              ),
+              child: Text(
+                'Build your career with mohammed mosad',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(.5),
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  FontAwesomeIcons.trash,
+                  size: 24,
+                  color: Colors.black,
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              right: 24,
+            ),
+            child: Text(
+              'May21,2023',
+              style: TextStyle(
+                fontSize: 12,
+                color: Colors.black.withOpacity(.4),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
