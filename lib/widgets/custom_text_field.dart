f4e8cef3cd0dfe66859c0ccb9da4d3fb57d5365c
Eng_Mosad <mohammedmosaad869@gmail.com>
2024-01-19 18:46:04 +0200

    edit notes view done

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -14,6 +14,7 @@ class CustomTextField extends StatelessWidget {
       decoration: InputDecoration(
         hintText: hntText,
         hintStyle: const TextStyle(
+          
           color: kprimaryColor,
         ),
         border: buildBorder(),
--- lib/widgets/edit_note_view_body.dart
+++ lib/widgets/edit_note_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
+import 'package:notes_app/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
@@ -18,6 +19,19 @@ class EditNoteViewBody extends StatelessWidget {
           CustomAppBar(
             text: 'Edit Note',
             icon: Icons.check,
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          CustomTextField(
+            hntText: 'Title',
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextField(
+            hntText: 'Content',
+            maxLines: 6,
           ),
         ],
       ),
